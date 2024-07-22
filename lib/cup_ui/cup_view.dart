428f9f188d3b5d3ad7bd952a58976a8cfce8af50
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-23 00:30:32 +0530

    Added code to login to addresses

--- lib/cup_ui/cup_view.dart
+++ lib/cup_ui/cup_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart' show DefaultMaterialLocalizations;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tempbox/bloc/data/data_bloc.dart';
+import 'package:tempbox/bloc/data/data_event.dart';
+import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/cup_ui/cup_addresses_list/cup_addresses_list.dart';
 
 const String title = 'TempBox';
@@ -22,8 +24,23 @@ class CupView extends StatelessWidget {
       theme: const CupertinoThemeData(primaryColor: Color(0xFFBA1F33)),
       home: MultiBlocProvider(
         providers: [BlocProvider<DataBloc>(create: (BuildContext context) => DataBloc())],
-        child: const CupAddressesList(),
+        child: const CupUIStarter(),
       ),
+    );
+  }
+}
+
+class CupUIStarter extends StatelessWidget {
+  const CupUIStarter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<DataBloc, DataState>(
+      buildWhen: (previous, current) => false,
+      builder: (dataBlocContext, dataState) {
+        BlocProvider.of<DataBloc>(dataBlocContext).add(const LoginToAccountsEvent());
+        return const CupAddressesList();
+      },
     );
   }
 }
