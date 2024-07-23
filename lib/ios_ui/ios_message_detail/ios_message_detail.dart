d0fb4f6ef920e04e551a58c2a1ef9491107938a5
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-23 16:11:31 +0530

    Added change to reset message selection on back navigation from ios_message_detail

--- lib/ios_ui/ios_message_detail/ios_message_detail.dart
+++ lib/ios_ui/ios_message_detail/ios_message_detail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mailtm_client/mailtm_client.dart';
 import 'package:tempbox/bloc/data/data_bloc.dart';
+import 'package:tempbox/bloc/data/data_event.dart';
 import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/services/ui_service.dart';
 import 'package:tempbox/shared/components/render_message.dart';
@@ -13,11 +14,20 @@ class IosMessageDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<DataBloc, DataState>(builder: (dataBlocContext, dataState) {
-      return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text(UiService.getMessageFromName(message)),
+      if (dataState.selectedMessage == null) {
+        return CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(middle: Text(UiService.getMessageFromName(message))),
+          child: const SizedBox.shrink(),
+        );
+      }
+      return PopScope(
+        onPopInvoked: (didPop) {
+          BlocProvider.of<DataBloc>(dataBlocContext).add(const ResetSelectedMessageEvent());
+        },
+        child: CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(middle: Text(UiService.getMessageFromName(dataState.selectedMessage!))),
+          child: RenderMessage(message: dataState.selectedMessage!),
         ),
-        child: RenderMessage(message: message, user: dataState.selectedAddress!.authenticatedUser),
       );
     });
   }
