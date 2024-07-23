a298762987f726805bcdc7464bbad7eee2613064
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-23 16:03:20 +0530

    updates message_detail for android

--- lib/android_views/message_detail/message_detail.dart
+++ lib/android_views/message_detail/message_detail.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mailtm_client/mailtm_client.dart';
-import 'package:tempbox/bloc/data/data_bloc.dart';
-import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/services/ui_service.dart';
 import 'package:tempbox/shared/components/render_message.dart';
 
@@ -12,11 +9,9 @@ class MessageDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DataBloc, DataState>(builder: (dataBlocContext, dataState) {
-      return Scaffold(
-        appBar: AppBar(title: Text(UiService.getMessageFromName(message))),
-        body: RenderMessage(message: message, user: dataState.selectedAddress!.authenticatedUser),
-      );
-    });
+    return Scaffold(
+      appBar: AppBar(title: Text(UiService.getMessageFromName(message))),
+      body: RenderMessage(message: message),
+    );
   }
 }
