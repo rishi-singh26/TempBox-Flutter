4b9b10d267c6f47207583fad91fa26a9f2bcf3e3
rishi-singh26 <rishisingh0831@gmail.com>
2024-07-24 21:02:06 +0530

    Added the macui_message_detail widget

---
+++ lib/macos_views/views/message_detail/macui_message_detail.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tempbox/bloc/data/data_bloc.dart';
+import 'package:tempbox/bloc/data/data_state.dart';
+import 'package:tempbox/shared/components/render_message.dart';
+
+class MacuiMessageDetail extends StatelessWidget {
+  const MacuiMessageDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<DataBloc, DataState>(
+      buildWhen: (previous, current) {
+        if (previous.selectedMessage?.id != current.selectedMessage?.id || previous.selectedMessage?.html != current.selectedMessage?.html) {
+          return true;
+        }
+        return false;
+      },
+      builder: (dataBlocContext, dataState) {
+        if (dataState.selectedAddress == null) {
+          return const Center(child: Text('No Address Selected'));
+        }
+        if (dataState.selectedMessage == null) {
+          return const Center(child: Text('No Message Selected'));
+        }
+        return RenderMessage(key: Key(dataState.selectedMessage!.id), message: dataState.selectedMessage!);
+      },
+    );
+  }
+}
