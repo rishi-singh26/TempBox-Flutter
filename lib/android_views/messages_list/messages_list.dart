113eeaeeef1b2421e4e116288369526e992bcce8
rishi-singh26 <rishisingh0831@gmail.com>
2024-08-30 21:26:04 +0530

    Added address info button on messages list screen on ios and android UI

--- lib/android_views/messages_list/messages_list.dart
+++ lib/android_views/messages_list/messages_list.dart
@@ -2,14 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:mailtm_client/mailtm_client.dart';
+import 'package:tempbox/android_views/address_info/address_info.dart';
 import 'package:tempbox/bloc/data/data_bloc.dart';
 import 'package:tempbox/bloc/data/data_event.dart';
 import 'package:tempbox/bloc/data/data_state.dart';
+import 'package:tempbox/models/address_data.dart';
+import 'package:tempbox/services/overlay_service.dart';
 import 'package:tempbox/services/ui_service.dart';
 import 'package:tempbox/android_views/messages_list/message_tile.dart';
 
 class MessagesList extends StatelessWidget {
   const MessagesList({super.key});
+
+  _openAddressInfoSheet(BuildContext context, BuildContext dataBlocContext, AddressData addressData) {
+    OverlayService.showOverLay(
+      context: context,
+      useSafeArea: true,
+      isScrollControlled: true,
+      clipBehavior: Clip.hardEdge,
+      enableDrag: true,
+      builder: (context) => BlocProvider.value(
+        value: BlocProvider.of<DataBloc>(dataBlocContext),
+        child: AddressInfo(addressData: addressData),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +43,15 @@ class MessagesList extends StatelessWidget {
             },
             child: CustomScrollView(
               slivers: [
-                SliverAppBar.large(title: Text(UiService.getAccountName(dataState.selectedAddress!))),
+                SliverAppBar.large(
+                  title: Text(UiService.getAccountName(dataState.selectedAddress!)),
+                  actions: [
+                    IconButton(
+                      onPressed: () => _openAddressInfoSheet(context, dataBlocContext, dataState.selectedAddress!),
+                      icon: const Icon(Icons.info_outline_rounded),
+                    ),
+                  ],
+                ),
                 const MessageList(),
               ],
             ),
--- lib/ios_ui/ios_messages_list/ios_messages_list.dart
+++ lib/ios_ui/ios_messages_list/ios_messages_list.dart
@@ -1,3 +1,4 @@
+import 'package:cupertino_modal_sheet/cupertino_modal_sheet.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
@@ -6,6 +7,7 @@ import 'package:tempbox/bloc/data/data_bloc.dart';
 import 'package:tempbox/bloc/data/data_event.dart';
 import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/ios_ui/colors.dart';
+import 'package:tempbox/ios_ui/ios_address_info/ios_address_info.dart';
 import 'package:tempbox/ios_ui/ios_messages_list/ios_message_tile.dart';
 import 'package:tempbox/models/address_data.dart';
 import 'package:tempbox/services/ui_service.dart';
@@ -15,6 +17,16 @@ class IosMessagesList extends StatelessWidget {
 
   _onRefresh(BuildContext dataBlocContext, AddressData address) async {
     BlocProvider.of<DataBloc>(dataBlocContext).add(GetMessagesEvent(addressData: address));
+  }
+
+  _openAddressInfoSheet(BuildContext context, BuildContext dataBlocContext, AddressData addressData) {
+    showCupertinoModalSheet(
+      context: context,
+      builder: (context) => BlocProvider.value(
+        value: BlocProvider.of<DataBloc>(dataBlocContext),
+        child: IosAddressInfo(addressData: addressData),
+      ),
+    );
   }
 
   @override
@@ -47,6 +59,10 @@ class IosMessagesList extends StatelessWidget {
               backgroundColor: AppColors.navBarColor,
               largeTitle: Text(UiService.getAccountName(dataState.selectedAddress!)),
               previousPageTitle: 'TempBox',
+              trailing: CupertinoButton(
+                onPressed: () => _openAddressInfoSheet(context, dataBlocContext, dataState.selectedAddress!),
+                child: const Icon(CupertinoIcons.info_circle),
+              ),
             ),
             const SliverToBoxAdapter(
               child: SizedBox(height: 400, child: Center(child: Text('No message available'))),
@@ -67,6 +83,10 @@ class IosMessagesList extends StatelessWidget {
                 largeTitle: Text(UiService.getAccountName(dataState.selectedAddress!)),
                 border: null,
                 previousPageTitle: 'TempBox',
+                trailing: CupertinoButton(
+                  onPressed: () => _openAddressInfoSheet(context, dataBlocContext, dataState.selectedAddress!),
+                  child: const Icon(CupertinoIcons.info_circle),
+                ),
               ),
               SliverList.separated(
                 separatorBuilder: (context, index) => Container(
