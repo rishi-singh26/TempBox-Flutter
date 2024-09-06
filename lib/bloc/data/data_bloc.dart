9eed5d07837109ead2b900f77a9251cd094e064f
rishi-singh26 <rishisingh0831@gmail.com>
2024-09-06 16:22:30 +0530

    Updated data_bloc to use custom login logic

--- lib/bloc/data/data_bloc.dart
+++ lib/bloc/data/data_bloc.dart
@@ -4,6 +4,7 @@ import 'package:mailtm_client/mailtm_client.dart';
 import 'package:tempbox/bloc/data/data_event.dart';
 import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/models/address_data.dart';
+import 'package:tempbox/services/http_service.dart';
 
 class DataBloc extends HydratedBloc<DataEvent, DataState> {
   DataBloc() : super(DataState.initial()) {
@@ -20,7 +21,7 @@ class DataBloc extends HydratedBloc<DataEvent, DataState> {
           if (address.isAddressActive) {
             AuthenticatedUser? loggedInUser;
             try {
-              loggedInUser = await MailTm.login(address: address.authenticatedUser.account.address, password: address.password);
+              loggedInUser = await HttpService.login(address.authenticatedUser.account.address, address.password);
             } catch (e) {
               debugPrint(e.toString());
             }
