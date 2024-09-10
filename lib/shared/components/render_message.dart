97a648e22c109c17d372daa3db9fd20bedb8dfae
rishi-singh26 <rishisingh0831@gmail.com>
2024-09-10 17:45:58 +0530

    Updated html rendered style - removed padding

--- lib/shared/components/render_message.dart
+++ lib/shared/components/render_message.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:tempbox/models/message_data.dart';
@@ -12,29 +14,32 @@ class RenderMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String headerHTML =
-        "<div style='display: flex; margin-left: 10px; margin-bottom: 10px;'><div style='display: flex; width: 40px; height: 40px; border-radius: 20px; background-color: #007AFF; align-items: center; justify-content: center; color: white; font-weight: bold;'>${UiService.getMessageFromName(message).substring(0, 1)}</div><div style='margin-left: 10px;'><div style='font-weight: bold;'>${UiService.getMessageFromName(message)}</div><a href='mailto:${message.from['address'] ?? ''}'>${message.from['address'] ?? ''}</a></div></div>";
+        "<div style='display: flex; margin-bottom: 10px;'><div style='display: flex; width: 40px; height: 40px; border-radius: 20px; background-color: #007AFF; align-items: center; justify-content: center; color: white; font-weight: bold;'>${UiService.getMessageFromName(message).substring(0, 1)}</div><div style='margin-left: 10px;'><div style='font-weight: bold;'>${UiService.getMessageFromName(message)}</div><a href='mailto:${message.from['address'] ?? ''}'>${message.from['address'] ?? ''}</a></div></div>";
 
     final html = message.html.isEmpty ? '<p>Loading...</p>' : headerHTML + message.html.join('');
-    return HtmlWidget(
-      html,
-      onTapUrl: (url) async {
-        bool? choice = await AlertService.getConformation<bool>(
-          context: context,
-          title: 'Do you want to open this URL?',
-          content: url,
-          secondaryBtnTxt: 'Copy',
-          truncateContent: true,
-        );
-        if (choice == true) {
-          await launchUrl(Uri.parse(url));
-        } else if (choice == false) {
-          UiService.copyToClipboard(url);
-        }
-        return true;
-      },
-      onTapImage: (p0) {},
-      enableCaching: true,
-      buildAsync: true,
+    return Padding(
+      padding: (Platform.isAndroid || Platform.isIOS) ? EdgeInsets.zero : const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: HtmlWidget(
+        html,
+        onTapUrl: (url) async {
+          bool? choice = await AlertService.getConformation<bool>(
+            context: context,
+            title: 'Do you want to open this URL?',
+            content: url,
+            secondaryBtnTxt: 'Copy',
+            truncateContent: true,
+          );
+          if (choice == true) {
+            await launchUrl(Uri.parse(url));
+          } else if (choice == false) {
+            UiService.copyToClipboard(url);
+          }
+          return true;
+        },
+        onTapImage: (p0) {},
+        enableCaching: true,
+        buildAsync: true,
+      ),
     );
   }
 }
