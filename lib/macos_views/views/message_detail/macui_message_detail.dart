1c1551437789df6bafb03f55031ec57c0f6ccd70
rishi-singh26 <rishisingh0831@gmail.com>
2024-09-10 17:45:38 +0530

    Added attachment supoort in mac_ui and win_ui

--- lib/macos_views/views/message_detail/macui_message_detail.dart
+++ lib/macos_views/views/message_detail/macui_message_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tempbox/bloc/data/data_bloc.dart';
 import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/models/message_data.dart';
+import 'package:tempbox/shared/components/attachment_card.dart';
 import 'package:tempbox/shared/components/render_message.dart';
 
 class MacuiMessageDetail extends StatelessWidget {
@@ -25,7 +26,22 @@ class MacuiMessageDetail extends StatelessWidget {
           return const Center(child: Text('No Message Selected'));
         }
         MessageData messageWithHtml = dataState.messageIdToMessageMap[dataState.selectedMessage!.id] ?? dataState.selectedMessage!;
-        return RenderMessage(key: Key(messageWithHtml.id), message: messageWithHtml);
+        return ListView(
+          children: [
+            RenderMessage(key: Key(messageWithHtml.id), message: messageWithHtml),
+            if (messageWithHtml.hasAttachments)
+              SizedBox(
+                height: 80,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: messageWithHtml.attachments.length,
+                  itemBuilder: (context, index) {
+                    return AttachmentCard(key: Key(messageWithHtml.attachments[index].id), attachment: messageWithHtml.attachments[index]);
+                  },
+                ),
+              )
+          ],
+        );
       },
     );
   }
--- lib/win_views/views/selected_address_view/winui_selected_address_view.dart
+++ lib/win_views/views/selected_address_view/winui_selected_address_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tempbox/bloc/data/data_bloc.dart';
 import 'package:tempbox/bloc/data/data_state.dart';
 import 'package:tempbox/models/message_data.dart';
+import 'package:tempbox/shared/components/attachment_card.dart';
 import 'package:tempbox/shared/components/render_message.dart';
 import 'package:tempbox/win_views/views/selected_address_view/winui_messages_list.dart';
 
@@ -44,7 +45,22 @@ class WinuiSelectedAddressView extends StatelessWidget {
               MessageData messageWithHtml = dataState.messageIdToMessageMap[dataState.selectedMessage!.id] ?? dataState.selectedMessage!;
               return ScaffoldPage(
                 padding: const EdgeInsets.symmetric(vertical: 3),
-                content: RenderMessage(key: Key(messageWithHtml.id), message: messageWithHtml),
+                content: ListView(
+                  children: [
+                    RenderMessage(key: Key(messageWithHtml.id), message: messageWithHtml),
+                    if (messageWithHtml.hasAttachments)
+                      SizedBox(
+                        height: 80,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: messageWithHtml.attachments.length,
+                          itemBuilder: (context, index) {
+                            return AttachmentCard(key: Key(messageWithHtml.attachments[index].id), attachment: messageWithHtml.attachments[index]);
+                          },
+                        ),
+                      )
+                  ],
+                ),
               );
             }),
           )
