668d0200bbc2033f1e5d25268ea61e10914594d1
rishi-singh26 <rishisingh0831@gmail.com>
2025-01-10 05:59:49 +0530

    Updated app version

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,5 +1,5 @@
 class AppConstatns {
-  static const appVersion = 'Version 1.2.3';
+  static const appVersion = 'Version 1.2.4';
   static const String resetAppData =
       'Are you sure you want to reset app data?\nThis will delete all addresses and all emails received on those addresses.\n\nThis action is irreversible.';
 }
